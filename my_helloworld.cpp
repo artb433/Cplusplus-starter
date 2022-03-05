8087a470355809063e9b82fe61ae1561bb2bf02f
Art b <94374097+artb433@users.noreply.github.com>
2022-03-05 23:04:29 +0000

    myHelloworld

---
+++ my_helloworld.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string name;
+    int age;
+    age=68;
+     cout<<"============================================================================================="<<endl;
+ cout<<"============================================================================================="<<endl;
+//  cout<<"What is the name;";
+//  cin>>name;
+//     cout<<"There once was a man named "<<name<<endl;
+//     cout<<"He was "<<age<<" years old"<<endl;
+//     cout<<"He liked the name "<<name<<endl;
+//     cout<<"But did not like being "<<age<<endl;
+//  cout << "Hello World!"<<endl;
+// char grade ='A';
+// cout<<grade<<endl;
+string phrase;
+phrase="Tahiru Abdul Rahman Baaba ";
+cout<<phrase.append(" Art b")<<endl;
+cout<<phrase.find("Art b")<<endl;
+cout<<phrase[1]<<endl;
+  cout << "Come up and C++ me sometime.";  // message
+  // start a new line
+  cout << "Here is the total: 1000.00\n";
+  cout << "Here we go!\n";
+  cout << "Start here!\n";
+  
+  
+  cout<<"Size of char="<<sizeof(char)<<endl;
+  cout<<"Size of int="<<sizeof(int)<<endl;
+  cout<<"Size of short="<<sizeof(short)<<endl;
+  cout<<"Size of unsigned int="<<sizeof(unsigned int)<<endl;
+  cout<<"Size of long="<<sizeof(long)<<endl;
+  cout<<"Size of long long="<<sizeof(long long)<<endl;
+  cout<<"Size of bool="<<sizeof(bool)<<endl;
+  cout<<"Size of float="<<sizeof(float)<<endl;
+  cout<<"Size of double="<<sizeof(double)<<endl;
+  cout<<"Size of long double="<<sizeof(long double)<<endl;
+  cout<<"Size of unsigned short="<<sizeof(unsigned short)<<endl;
+  cout<<"Size of unsigned long="<<sizeof(unsigned long)<<endl;
+  cout<<"============================================================================================"<<endl;
+ cout<<"============================================================================================"<<endl;
+//  cout<<"     /| "<<endl;
+//  cout<<"    / | "<<endl;
+//  cout<<"   /  | "<<endl;
+//  cout<<"  /   | "<<endl;
+//  cout<<" /____| "<<endl;
+}
